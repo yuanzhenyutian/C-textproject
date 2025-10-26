309116df14e54943ba71cf4b49c89e956ab4b87c
dawei <david43014175@gmail.com>
2025-10-26 10:06:23 +0100

    练习文件第一次提交

---
+++ Project_2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Phone
+{
+public:
+	string m_pname;
+	Phone(string pname)
+	{
+		m_pname = pname
+	}
+};
+
+class Person
+{
+
+public:
+	string m_name;
+	Phone m_phone;
+};
+
+int main()
+{
+
+
+}
