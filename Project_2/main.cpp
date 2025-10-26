c46bd14616152031c42cd47e51a4caed4ac7d6f2
dawei <david43014175@gmail.com>
2025-10-26 11:25:30 +0100

    静态成员变量案例

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -2,34 +2,34 @@
 #include <string>
 using namespace std;
 
-class Phone
-{
-public:
-	string m_pname;
-	Phone(string pname)
-	{
-		m_pname = pname;
-	}
-};
-
 class Person
 {
 public:
-	Person(string name, string pname) 
-		:m_name(name),m_phone(pname)
-	{
-    }
-	
-	string m_name;
-	Phone m_phone;
+	static int m_a;
 };
+
+int Person::m_a = 100;
+
 void test01()
 {
-	Person p("冬冬", "华为MATE");
+	Person p;
+	cout << p.m_a << endl;
+	Person p1;
+	p.m_a = 200;
+	cout << p.m_a << endl;
+}
+void text2()
+{
+
+	/*Person p;
+	cout << p.m_a<<endl;*/
+	cout << Person::m_a << endl;
 }
 
 int main()
-{
+{  
 
-	test01();
+	/*test01();*/
+	text2();
+
 }
