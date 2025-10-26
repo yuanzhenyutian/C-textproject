5e0ef0ece91669d7333cc6239a192d3652d232a7
dawei <david43014175@gmail.com>
2025-10-26 10:35:00 +0100

    二次提交文件修改内容

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -8,20 +8,28 @@ public:
 	string m_pname;
 	Phone(string pname)
 	{
-		m_pname = pname
+		m_pname = pname;
 	}
 };
 
 class Person
 {
-
 public:
+	Person(string name, string pname) 
+		:m_name(name),m_phone(pname)
+	{
+    }
+	
 	string m_name;
 	Phone m_phone;
 };
+void test01()
+{
+	Person p("冬冬", "华为MATE");
+}
 
 int main()
 {
 
-
+	test01();
 }
