723e255ca351fecec918a21b713b73c97b4bf272
dawei <david43014175@gmail.com>
2025-10-31 12:47:48 +0100

    计算机组装多态实现案例 练习

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -3,88 +3,155 @@
 using namespace std;
 //多态案例2 制作饮品
 
-class Drink
+class Cpu                                 //CPU
 {
 public:
-	virtual void Boil() = 0; //煮水
-	virtual void Brew() = 0;//冲泡 
-	virtual void PourInCup() = 0;//倒入杯中
-	virtual void PutSomething() = 0; //加入辅料
+	virtual void Calculate() = 0;   
+};
+class  VideoCard                         //显卡
+{
+public:
+	virtual void Display() =  0;
+};
+class Memory                            //内存
+{
+public:
+	virtual void Storage() = 0;
+};
 
-	void MakeDringk()
+//组装电脑  提供接口 工作方式
+class Computer
+{
+public:
+	Computer(Cpu *cp,VideoCard *vcard,Memory *mem)  //组装电脑
 	{
-		Boil();
-		Brew();
-		PourInCup();
-		PutSomething();
-		
+		m_cp = cp;
+		m_vcard = vcard;
+		m_mem = mem;
+	}
+
+	void dowork()  //工作函数
+	{
+		m_cp->Calculate();
+		m_vcard->Display();
+		m_mem->Storage();
+	} 
+	//提供析构函数  释放三个电脑零件的堆区内存
+	~Computer()
+	{
+		//释放组件
+		if (m_cp != nullptr)
+		{
+			delete m_cp;
+			m_cp = nullptr;
+		}
+		if (m_vcard != nullptr)
+		{
+			delete m_vcard;
+			m_vcard = nullptr;
+		}
+		if (m_mem != nullptr)
+		{
+			delete m_mem;
+			m_mem = nullptr;
+		}
 	}
 	
+private:
+	Cpu* m_cp;
+	VideoCard *m_vcard;
+	Memory* m_mem;
 };
 
-class Coff :public Drink  //咖啡
+
+//提供零件厂商
+class InterCpu: public Cpu
 {
 public:
-
-	virtual void Boil()
+	void Calculate()
 	{
-		cout << "1--煮水，做好准备" << endl;
+		cout << "Inter厂商提供的CPU在计算" << endl;
 	}
-	virtual void Brew() 
-	{
-		cout << "2--冲泡咖啡" << endl;
-	}
-	virtual void PourInCup()
-	{
-		cout << "3--倒入杯中" << endl;
-	}
-	virtual void PutSomething() 
-	{
-		cout << "4--加入糖和牛奶" << endl;
-	}
-	
 };
-class Tea :public Drink
+class InterVideoCard :public VideoCard
 {
 public:
-	virtual void Boil()
+	void Display()
 	{
-		cout << "1--煮水，做好准备" << endl;
-	}
-	virtual void Brew()
-	{
-		cout << "2--冲泡茶叶" << endl;
-	}
-	virtual void PourInCup()
-	{
-		cout << "3--倒入杯中" << endl;
-	}
-	virtual void PutSomething()
-	{
-		/*cout << "4--加入柠檬" << endl;*/
+		cout << "Inter厂商提供的display在显示" << endl;
 	}
 };
-void dowork(Drink* drink)
+class InterMemory :public Memory
 {
+public:
+	void Storage() 
+	{
+		cout << "Inter厂商提供的storage在存储" << endl;
+	}
+};
 
-	drink->MakeDringk();
-	delete drink;
-}
+
+//lenovo 厂商
+class LenovoCpu :public Cpu
+{
+public:
+	virtual	void Calculate()
+	{
+		cout << "Lenovo厂商提供的CPU在计算" << endl;
+	}
+};
+class LenovoVideoCard :public VideoCard
+{
+public:
+	virtual void Display()
+	{
+		cout << "Lenovo厂商提供的display在显示" << endl;
+	}
+};
+class LenovoMemory :public Memory
+{
+public:
+	virtual void Storage()
+	{
+		cout << "Lenovo厂商提供的storage在存储" << endl;
+	}
+};
+//测试组装电脑
 void test01()
 {
-	//制作咖啡
-	dowork(new Coff);
+	//第一台电脑
+	cout << "第一台电脑在正常的工作........\n" << endl;
+	Cpu* intercpu = new InterCpu;
+	VideoCard* interVideoCard = new InterVideoCard;
+	Memory* memory = new InterMemory;
 
-	cout << "-----***-----***-----***-----" << endl;
-	
-	dowork(new Tea);
+	//创建第一台电脑  耦合设计......
+    Computer* computer1 = new Computer(intercpu, interVideoCard, memory);
+	//开始工作
+
+	computer1->dowork();
+    delete computer1;
+	cout << "--------------------------------\n" << endl;
+	//第二台电脑的组装
+	cout << "第二台电脑在正常的工作........、\n" << endl;
+	Computer* computer2 =new Computer(new LenovoCpu, new LenovoVideoCard,new LenovoMemory  );
+	//开始工作
+
+	computer2->dowork();
+	delete computer2; 
+
+	cout << "第三台电脑在正常的工作........、\n" << endl;
+	Computer* computer3 = new Computer(new LenovoCpu, new InterVideoCard, new LenovoMemory);
+	//开始工作
+
+	computer3->dowork();
+	delete computer3;
 
 } 
 
+ 
 int main()
 {  
-
-	test01();
+	test01(); 
 	system("pause");
 }
- 
