8eafb450fd1ae6a223781a335b6a8a3d97634dc7
dawei <david43014175@gmail.com>
2025-11-03 12:39:38 +0100

    读文件练习

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -3,23 +3,36 @@
 #include <fstream>
 
 using namespace std;
-//文件操作
 
-void text01()
+class Person
 {
-	ofstream ofs;   //创建输出对象
+public:
 
-	ofs.open("message.txt", ios::binary);   //打开文件  指定文件  打开方式
+    char m_name[64];
+    int m_age;
+};
 
-	ofs << "姓名：   张三" << endl;
-	ofs << "性别：   男" << endl;
-	ofs << "年龄：   35" << endl;
+void text1()
+{
+    ifstream ifs;
+    ifs.open("main.txt", ios::in | ios::binary);
+    if (!ifs.is_open())
+    {
+        cout << "无法打开文件......" << endl;
 
-	ofs.close();
+        return;
+    }
+    Person p;
+
+    ifs.read((char*)&p, sizeof(p));
+
+    cout << "姓名 ：" << p.m_name << "年龄 ：" << p.m_age << endl;
+
+    ifs.close();
 
 }
- 
-int main()
+
+ int main()
 {  
-	text01();
+    text1();
 }
